5460dc2f3eedb8260ee234ab75201bf92e5d2bff
swallowtail62 <swallowtail.a7@gmail.com>
2023-07-06 12:16:30 +0900

    chore: Add re-production code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,48 @@ import { css } from "../../styled-system/css";
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
-      Hello 🐼!
+    <div
+      className={css({
+        minH: "100vh",
+        display: "grid",
+        gridTemplateRows: "auto 1fr",
+      })}
+    >
+      <header className={css({ bgColor: "gray.100", h: 12 })}>
+        some header
+      </header>
+      <div
+        className={css({ display: "grid", gridTemplateColumns: "auto 1fr" })}
+      >
+        <aside className={css({ w: 80, bgColor: "blue.100" })}>
+          sidebar navigation
+        </aside>
+        <div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
+          Hello 🐼!
+        </div>
+      </div>
     </div>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <div
+//       style={{
+//         minHeight: "100vh",
+//         display: "grid",
+//         gridTemplateRows: "auto 1fr",
+//       }}
+//     >
+//       <header style={{ backgroundColor: "gray", height: "48px" }}>
+//         some header
+//       </header>
+//       <div style={{ display: "grid", gridTemplateColumns: "auto 1fr" }}>
+//         <aside style={{ width: "320px", backgroundColor: "skyblue" }}>
+//           sidebar navigation
+//         </aside>
+//         <div style={{ fontSize: "2xl", fontWeight: "bold" }}>Hello 🐼!</div>
+//       </div>
+//     </div>
+//   );
+// }
